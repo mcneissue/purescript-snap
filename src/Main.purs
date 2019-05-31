b9dee2760bb2793bfab9bd30e646c21429b346fe
Asad Saeeduddin <masaeedu@gmail.com>
2019-05-31 07:02:15 -0400

    Inline composite component definition into Main

--- src/Main.purs
+++ src/Main.purs
@@ -10,9 +10,9 @@ import Effect.Class (liftEffect)
 import Effect.Exception (throwException)
 import Effect.Ref as Ref
 import Snap (snap)
-import Snap.Component (contraHoistVoid)
+import Snap.Component (contraHoistVoid, refocusAll, squash)
 import Snap.React (reactTarget, refSnapper)
-import Snap.React.Component (twoCountersAndAnInput)
+import Snap.React.Component (counter, input, text, divved)
 import Web.DOM (Element)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
@@ -26,11 +26,16 @@ element = do
 
 main :: Effect Unit
 main = do
-  let s0 = { counter1: 0, counter2: 42, input: "Hello" }
+  let s0 = { counter1: 0, counter2: 42, input: "Hello, World" }
+  let component = contraHoistVoid launchAff_
+                $  (squash <<< refocusAll) { counter1: counter }
+                <> (squash <<< refocusAll) { counter2: counter }
+                <> (squash <<< refocusAll) { input }
+                <> divved text
   e <- element
   ref <- liftEffect $ Ref.new s0
   launchAff_ $ do
     av  <- AVar.empty
     let snapper = refSnapper ref av
     let target = reactTarget e av
-    snap snapper (contraHoistVoid launchAff_ twoCountersAndAnInput) target
+    snap snapper component target
