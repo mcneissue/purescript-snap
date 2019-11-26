36c0153ee2f016bcef437f71f68622b85e19a715
John C. Lavelle <jlavelle604@gmail.com>
2019-11-26 13:06:11 -0500

    State for routing example

--- examples/routing/src/Routing/State.purs
+++ examples/routing/src/Routing/State.purs
@@ -2,5 +2,28 @@ module Examples.Routing.State where
 
 import Prelude
 
-initialState :: Unit
-initialState = unit
+import Examples.CatTron.State as CatTron
+import Examples.Reducer.State as Reducer
+import Examples.Routing.Router as Router
+import Examples.TodoMVC.State as TodoMvc
+import Examples.TransactionalForm.State as Transactional
+
+data RouteState
+  = Root
+  | TodoMvc TodoMvc.App
+  | CatTron CatTron.State
+  | Reducer Reducer.State
+  | Transactional Transactional.State
+
+type State = RouteState
+
+initialState :: State
+initialState = Root
+
+stateFor :: Router.Route -> State
+stateFor r = case r of
+  Router.Root -> Root
+  Router.CatTron -> CatTron CatTron.initialState
+  Router.Reducer -> Reducer Reducer.initialState
+  Router.TodoMvc -> TodoMvc TodoMvc.initialState
+  Router.Transactional -> Transactional Transactional.initialState
