37fa2e6dc90f4c19b4ad0fc4af5280e00a1bcb4c
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-21 15:55:54 -0400

    Add hoist operation for targets

--- src/Snap.purs
+++ src/Snap.purs
@@ -14,6 +14,11 @@ newtype Target m v = Target (v -> m (Target m v))
 instance contravariantTarget :: Functor m => Contravariant (Target m) where
   cmap f (Target a) = Target \v -> cmap f <$> a (f v)
 
+hoistTarget :: forall m n v. Functor n => (m ~> n) -> Target m v -> Target n v
+hoistTarget n = go
+  where
+  go (Target t) = Target $ map go <<< n <<< t
+
 snap :: forall m v x
       . Monad m
      => Cmp m v Unit Void
