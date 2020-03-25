1b241aa9d9dc4fddc21b42a0916067239979a0ee
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-25 06:41:29 -0400

    Add affSnapper, affSnapper_

--- src/Snap/React/Snapper.purs
+++ src/Snap/React/Snapper.purs
@@ -2,6 +2,7 @@ module Snap.React.Snapper where
 
 import Prelude
 
+import Control.Monad.Reader (class MonadAsk, ask)
 import Data.Either (Either)
 import Data.Profunctor (dimap)
 import Effect (Effect)
@@ -14,6 +15,7 @@ import Effect.Ref as Ref
 import Routing.Duplex (RouteDuplex, parse, print)
 import Routing.Duplex.Parser (RouteError)
 import Routing.Hash (getHash, setHash)
+import Snap (reduced)
 import Snap.Snapper (Snapper(..), Snapper')
 
 refSnapper :: forall s m. MonadAff m => Ref s -> AVar Unit -> Snapper m s s
@@ -24,6 +26,18 @@ refSnapper ref sync = Snapper { get, put }
     liftEffect $ Ref.write u ref
     _ <- liftAff $ AVar.put unit sync
     pure unit
+
+affSnapper :: forall m u s.
+  MonadAff m =>
+  (u -> s -> m s) -> s -> AVar Unit -> m (Snapper m u s)
+affSnapper red s sync = do
+  r <- liftEffect $ Ref.new s
+  pure $ reduced red $ refSnapper r sync
+
+affSnapper_ :: forall m s.
+  MonadAff m =>
+  s -> AVar Unit -> m (Snapper m s s)
+affSnapper_ = affSnapper $ \s _ -> pure s
 
 url :: Snapper' Effect String
 url = Snapper { get: getHash, put: setHash }
