557ba6284c7e4fca43209bda2ae857db0c66fde5
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-25 06:38:23 -0400

    Polymorphize encapsulate

--- src/Snap.purs
+++ src/Snap.purs
@@ -9,5 +9,5 @@ import Snap.Target as T
 import Snap.Component.SYTC (Cmp)
 import Snap.Snapper (Snapper(..))
 
-encapsulate :: forall m v s u. Monad m => Snapper m u s -> Cmp m v s u -> Cmp m (m v) Unit Void
+encapsulate :: forall m v s u x y. Monad m => Snapper m u s -> Cmp m v s u -> Cmp m (m v) x y
 encapsulate (Snapper { get, put }) cmp _ _ = get <#> cmp put
