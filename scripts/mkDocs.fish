5f17c155cc97f6da5c2456f9d9f2e1b4f89c529a
John C. Lavelle <jlavelle604@gmail.com>
2020-10-18 21:00:08 -0400

    Generated docs get copied to docs folder

--- scripts/mkDocs.fish
+++ scripts/mkDocs.fish
@@ -1,3 +1,8 @@
 set root (realpath (dirname (status --current-filename)))/..
+echo Deleting old documentation in $root/docs
+rm -rf $root/docs
 cd $root/examples
 spago docs
+echo Copying generated documentation to $root/docs
+rsync -a $root/examples/generated-docs/html/ $root/docs/
+echo Done.
