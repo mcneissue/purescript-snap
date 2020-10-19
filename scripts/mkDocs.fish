049738df04e6cf6c3a434c4758d28684534c48a2
John C. Lavelle <jlavelle604@gmail.com>
2020-10-19 15:33:43 -0400

    Docs from mealy branch

--- scripts/mkDocs.fish
+++ scripts/mkDocs.fish
@@ -1,3 +1,26 @@
 set root (realpath (dirname (status --current-filename)))/..
+set currentBranch (git branch --show-current)
+set msg "Generated docs "(date "+%m/%d/%y %T")" for branch $currentBranch"
+
+echo Deleting old documentation in $root/docs
+
+rm -rf $root/docs
+
 cd $root/examples
 spago docs
+
+git stash
+git checkout gh-pages
+or exit
+git stash pop
+
+echo Copying generated documentation to $root/docs
+
+rsync -a $root/examples/generated-docs/html/ $root/docs/
+
+git add -f $root/docs/.
+
+git commit -m $msg
+and git push
+
+git checkout $currentBranch
