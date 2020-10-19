099a186c6eaca06213c62d07cb26bc47db552bc2
John C. Lavelle <jlavelle604@gmail.com>
2020-10-19 15:34:47 -0400

    Update docs script

--- scripts/mkDocs.fish
+++ scripts/mkDocs.fish
@@ -1,9 +1,12 @@
 set root (realpath (dirname (status --current-filename)))/..
+cd $root
+
+set changes (git status --porcelain)
+test -z "$changes"
+or begin; echo Working branch must be clean to generate docs.; exit; end
+
 set currentBranch (git branch --show-current)
 set msg "Generated docs "(date "+%m/%d/%y %T")" for branch $currentBranch"
-
-git checkout gh-pages
-or exit
 
 echo Deleting old documentation in $root/docs
 
@@ -12,7 +15,13 @@ rm -rf $root/docs
 cd $root/examples
 spago docs
 
+git stash
+git checkout gh-pages
+or exit
+git stash pop
+
 echo Copying generated documentation to $root/docs
+
 rsync -a $root/examples/generated-docs/html/ $root/docs/
 
 git add -f $root/docs/.
