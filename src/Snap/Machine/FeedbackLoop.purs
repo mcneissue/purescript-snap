5333d9b1895ac31775e27048271e1bcbf4e6b57f
Asad Saeeduddin <masaeedu@gmail.com>
2021-02-06 22:25:57 -0500

    More fixings of great good

--- src/Snap/Machine/FeedbackLoop.purs
+++ src/Snap/Machine/FeedbackLoop.purs
@@ -5,9 +5,11 @@ import Prelude
 import Control.Monad.Cont (ContT(..), runContT)
 import Data.Either (Either, either)
 import Data.Maybe (maybe)
+import Data.Time.Duration (Milliseconds(..))
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_, error)
+import Effect.Aff (Aff, delay, error, launchAff_)
+import Effect.Aff.Class (class MonadAff, liftAff)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Exception (throwException)
 import React.Basic (JSX)
@@ -46,7 +48,7 @@ emptyCont :: forall x f a. Applicative f => Monoid x => ContT x f a
 emptyCont = ContT \_ -> pure mempty
 
 encapsulate :: forall m v s u.
-  Apply m =>
+  MonadAff m =>
   FeedbackLoop Unit m s u -> 
   Cmp m v s u ->
   s -> ContT Unit m v
@@ -56,7 +58,7 @@ encapsulate machine cmp = loop
     case machine s of
       task /\ transition ->
         let recurse u = runContT (loop $ transition u) cb
-        in cb (cmp recurse s) *> runContT task recurse
+        in liftAff (delay $ Milliseconds 0.0) *> cb (cmp recurse s) *> runContT task recurse
 
 runReact :: forall m. MonadEffect m => Element -> ContT Unit m JSX -> m Unit
 runReact e (ContT f) = f \v -> liftEffect $ React.render v e
