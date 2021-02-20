a782c15b21ba4e4ba929d600e85a87288f11c030
Asad Saeeduddin <masaeedu@gmail.com>
2021-02-20 01:38:34 -0500

    Simplify composition a bit, witness monoidal functor for Transition

--- src/Snap/Machine/SYTC.purs
+++ src/Snap/Machine/SYTC.purs
@@ -2,14 +2,16 @@ module Snap.Machine.SYTC where
 
 import Prelude hiding (unit)
 
+import Control.Biapply ((<<*>>))
 import Control.K as K
-import Data.Bifunctor (bimap, lmap)
+import Data.Bifunctor (lmap)
 import Data.Either (Either(..), either)
 import Data.Either.Nested (type (\/))
 import Data.Profunctor (lcmap)
+import Data.Tuple (uncurry)
 import Data.Tuple.Nested (type (/\), (/\))
-import Snap.Machine.Step (Step, Transition(..))
-import Snap.Machine.Step (Step, Transition(..), foldTransition, mapS) as Step
+import Snap.Machine.Step (Step, Transition(..), pureish, kcomposeish)
+import Snap.Machine.Step (foldTransition, mapS) as Step
 
 type Machine s i e = s -> Step s i e
 
@@ -18,7 +20,7 @@ type EMachine s i = Machine s i (K.EK i)
 
 type MMachine m s i = Machine s i (m i)
 
-mapS :: forall s s' e i. (s -> s') -> (s' -> s) -> Machine s i e -> Machine s' i e
+mapS :: ∀ s s' e i. (s -> s') -> (s' -> s) -> Machine s i e -> Machine s' i e
 mapS f g = (map <<< map <<< lmap $ f) <<< lcmap g
 
 mapI :: ∀ s i i' e. (i' -> i) -> Machine s i e -> Machine s i' e
@@ -27,27 +29,28 @@ mapI = map <<< lcmap
 mapE :: ∀ s i e e'. (e -> e') -> Machine s i e -> Machine s i e'
 mapE = map <<< map <<< map
 
-splice :: forall s1 s2 i1 i2 e1 e2.
+splice :: ∀ s1 s2 i1 i2 e1 e2.
   Machine s1 i1 e1 -> Machine s2 i2 e2 -> Machine (s1 /\ s2) (i1 \/ i2) (e1 \/ e2)
 splice m1 m2 (s1 /\ s2) = case m1 s1 /\ m2 s2 of
   step1 /\ step2 -> either
     (Step.foldTransition (\s1' e1 -> Yes (s1' /\ s2 ) (Left e1 )) No <<< step1)
     (Step.foldTransition (\s2' e2 -> Yes (s1  /\ s2') (Right e2)) No <<< step2)
 
-compose :: forall s1 s2 a b c. Machine s1 a b -> Machine s2 b c -> Machine (s1 /\ s2) a c
-compose m1 m2 (s1 /\ s2) = case m1 s1 /\ m2 s2 of
-  step1 /\ step2 -> \a ->
-    Step.foldTransition (\s1' b -> Step.foldTransition (\s2' c -> Yes (s1' /\ s2') c) No $ step2 b) No $ step1 a
-
-unit :: forall e. Machine Unit Void e
+unit :: ∀ e. Machine Unit Void e
 unit = const absurd
 
-reportState :: forall s i e. Machine s i e -> Machine s i (s /\ e)
+compose :: ∀ s1 s2 a b c. Machine s1 a b /\ Machine s2 b c -> Machine (s1 /\ s2) a c
+compose m1m2 = uncurry kcomposeish <<< (m1m2 <<*>> _)
+
+identity :: ∀ a. Machine Unit a a
+identity = pure pureish
+
+reportState :: ∀ s i e. Machine s i e -> Machine s i (s /\ e)
 reportState m s i = Step.foldTransition (\s' e -> Yes s' (s' /\ e)) No $ m s i
 
 newtype Behavior i e = Behavior (Step (Behavior i e) i e)
 
-unfold :: forall s i e. Machine s i e -> s -> Behavior i e
+unfold :: ∀ s i e. Machine s i e -> s -> Behavior i e
 unfold m = go
   where
   go s = Behavior (Step.mapS go $ m s)
--- src/Snap/Machine/Step.purs
+++ src/Snap/Machine/Step.purs
@@ -2,9 +2,21 @@ module Snap.Machine.Step where
 
 import Prelude
 
+import Data.Tuple.Nested (type (/\), (/\))
 import Data.Bifunctor (class Bifunctor, lmap)
 
-data Transition s e = Yes s e | No
+data Transition s e = No | Yes s e
+
+joinish :: ∀ s1 s2 e. Transition s1 (Transition s2 e) -> Transition (s1 /\ s2) e
+joinish No = No
+joinish (Yes _ No) = No
+joinish (Yes s1 (Yes s2 e)) = Yes (s1 /\ s2) e
+
+pureish :: ∀ e. e -> Transition Unit e
+pureish = Yes unit
+
+kcomposeish :: ∀ s1 s2 a b c. (a -> Transition s1 b) -> (b -> Transition s2 c) -> a -> Transition (s1 /\ s2) c
+kcomposeish f g = joinish <<< map g <<< f
 
 instance functorTransition :: Functor (Transition s) where
   map f No = No
@@ -14,12 +26,12 @@ instance bifunctorTransition :: Bifunctor Transition where
   bimap _ _ No = No
   bimap f g (Yes s e) = Yes (f s) (g e)
 
-foldTransition :: forall s e r. (s -> e -> r) -> r -> Transition s e -> r
+foldTransition :: ∀ s e r. (s -> e -> r) -> r -> Transition s e -> r
 foldTransition yes no = case _ of
   No -> no
   Yes s e -> yes s e
 
 type Step s i e = i -> Transition s e
 
-mapS :: forall s s' i e. (s -> s') -> Step s i e -> Step s' i e
+mapS :: ∀ s s' i e. (s -> s') -> Step s i e -> Step s' i e
 mapS f s i = lmap f $ s i
